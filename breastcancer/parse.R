1637cbaabe71e1da88ac656599a07af46e1b58b4
nosyarg <york17a@ncssm.edu>
2016-08-25 13:57:36 -0400

    finished breast cancer parsing script

--- breastcancer/parse.R
+++ breastcancer/parse.R
@@ -1,4 +1,5 @@
 rm(list=ls())
-read.csv('wdb.data') -> DATA
-DATA[2] = as.numeric(DATA[2] == 'M')
-DATA[1] = NULL
+DATA <- read.csv('wdb.data')
+DATA[2] <- as.numeric(DATA[2] == 'M')
+DATA[1] <- NULL
+colnames(DATA) <- c('prog','radius.mean','radius.sd','radius.worst','texture.mean','texture.sd','texture.worst','perimeter.mean','perimeter.sd','perimeter.worst','area.mean','area.sd','area.worst','smoothness.mean','smoothness.sd','smoothness.worst','compactness.mean','compactness.sd','compactness.worst','concavity.mean','concavity.sd','concavity.worst','concavepoints.mean','concavepoints.sd','concavepoints.worst','symmetry.mean','symmetry.sd','symmetry.worst','fractaldimension.mean','fractaldimension.sd','fractaldimension.worst')
