865b75c4f6195f12cfe5a08056b147b634c786d8
nosyarg <york17a@ncssm.edu>
2016-08-25 14:05:24 -0400

    changed DATA to data

--- handwriting/handwritingdatagen.R
+++ handwriting/handwritingdatagen.R
@@ -3,5 +3,5 @@ source('parse.r')
 load_mnist()
 rbind(data.frame(test[2]),data.frame(train[2])) -> X
 rbind(data.frame(test[3]),data.frame(train[3])) -> y
-DATA <- cbind(X,y)
-colnames(DATA)[785] <- "prog"
+data <- cbind(X,y)
+colnames(data)[785] <- "prog"
